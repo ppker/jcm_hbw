c6a2bbc41f17d4e3216a0ae4138fa8e895eaf124
yata <yata818@163.com>
2018-07-02 20:01:55 +0800

    update

--- HuoBi.py
+++ HuoBi.py
@@ -2,9 +2,15 @@
 # -*- coding: utf-8 -*-
 
 from rest_hb import HuobiServices as hb
-import pymysql
+import pymysql, time
 
 class HuoBi(object):
+
+    db = None
+    cursor = None
+    def __init__(self):
+        self.db = pymysql.connect(host="140.143.32.44", user="root", password="yata123", db="yata_data_01", port=3306, charset="utf8")
+        self.cursor = self.db.cursor()
 
     def test(self):
         data = hb.get_symbols()
@@ -16,8 +22,17 @@ class HuoBi(object):
             'size': 10,
         }
         data = hb.get_history_trade(params)
-        print(type(data))
-        return data
+        if not None:
+            for item in data['data']:
+                ts_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(item['ts'] / 1000))
+                now_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+                use_sql = '''insert into hb_base_detail (symbol, amount, price, direction, ts, created_at, updated_at) 
+values ('%s', %f, %f, '%s', '%s', '%s', '%s') ''' % (item['symbol'], item['amount'], item['price'],
+                                                     item['direction'], ts_time, now_time)
+                self.cursor.execute(use_sql)
+                self.db.commit()
+
+        return 'ok'
 
 
 if __name__ == '__main__':

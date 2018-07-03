1e12d271c079cbe549186e02f7d5ebb1145ea093
yata <yata818@163.com>
2018-07-03 10:34:49 +0800

    test

--- HuoBi.py
+++ HuoBi.py
@@ -4,12 +4,14 @@
 from rest_hb import HuobiServices as hb
 import pymysql, time
 
-class HuoBi(object):
 
+class HuoBi(object):
     db = None
     cursor = None
+
     def __init__(self):
-        self.db = pymysql.connect(host="140.143.32.44", user="root", password="yata123", db="yata_data_01", port=3306, charset="utf8")
+        self.db = pymysql.connect(host="140.143.32.44", user="root", password="yata123", db="yata_data_01", port=3306,
+                                  charset="utf8")
         self.cursor = self.db.cursor()
 
     def test(self):
@@ -26,7 +28,6 @@ class HuoBi(object):
             for item in data['data']:
                 item = item['data'][0]
 
-
                 ts_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(round(int(item['ts']) / 1000)))
                 now_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
                 use_sql = '''insert into hb_base_detail (symbol, amount, price, direction, ts, created_at, updated_at) 
@@ -37,10 +38,16 @@ values ('%s', %f, %f, '%s', '%s', '%s', '%s') ''' % ('btcusdt', item['amount'], 
 
         return 'ok'
 
+    def get_kline(self):
+        data = hb.get_kline('btcusdt', '1min', 10)
+        print(data)
+        return
+
 
 if __name__ == '__main__':
     Hb = HuoBi()
     # HuoBi().test()
 
-    data = Hb.get_history_trade()
-    print(data)
+    # data = Hb.get_history_trade()
+    # print(data)
+    Hb.get_kline()
